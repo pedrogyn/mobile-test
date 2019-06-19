24adcf61452c7c09210e43332c75ed3af809716d
PEDRO PAULO DUARTE SILVA - MacBook Pro <pedro.paulods@hotmail.com>
2019-06-18 23:34:05 -0300

    changes to fix styles

--- src/components/WinnerComponent.jsx
+++ src/components/WinnerComponent.jsx
@@ -1,51 +1,60 @@
 import React, { Component } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { WHITE } from "../constants/colors";
 import LottieView from "lottie-react-native";
 
 export class WinnerComponent extends Component {
+  renderAnimation(gameWinner) {
+    if (gameWinner == "Player 1 Wins" || gameWinner == "Player 2 Wins") {
+      return (
+        <LottieView source={require("../animations/trophy.json")} autoPlay />
+      );
+    }
+    if (gameWinner == "Round Draw") {
+      return (
+        <LottieView source={require("../animations/draw.json")} autoPlay />
+      );
+    }
+
+    return null;
+  }
   render() {
     let { gameWinner } = this.props;
     return (
-      <View
-        style={{
-          flex: 1,
-          width: "100%",
-          height: "100%",
-          position: "absolute",
-          top: 0,
-          left: 0,
-          display: this.props.gameWinner ? "flex" : "none",
-          // display: "none",
-          backgroundColor: "rgba(0,0,0,0.5)"
-        }}
-      >
+      <View style={styles.container}>
         <TouchableOpacity
           activeOpacity={1}
           onPress={this.props.dismiss}
-          style={{
-            flex: 1,
-            // justifyContent: "center"
-            alignItems: "center"
-          }}
+          style={styles.background}
         >
-          <Text
-            style={{
-              fontSize: 28,
-              textTransform: "uppercase",
-              color: WHITE,
-              marginTop: 40
-            }}
-          >
-            {this.props.gameWinner}
-          </Text>
-          <LottieView
-            source={require("../animations/trophy.json")}
-            autoPlay
-            loop
-          />
+          <Text style={styles.text}>{gameWinner}</Text>
+
+          {this.renderAnimation(gameWinner)}
         </TouchableOpacity>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    display: gameWinner ? "flex" : "none",
+    backgroundColor: "rgba(0,0,0,0.5)"
+  },
+  background: {
+    flex: 1,
+    alignItems: "center"
+  },
+  text: {
+    fontSize: 28,
+    textTransform: "uppercase",
+    color: WHITE,
+    marginTop: 40
+  }
+});
